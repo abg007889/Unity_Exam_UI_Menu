3f758a28678beeb3f35783dec72376389a9d410a
Cloud1103 <abg007889@yahoo.com.hk>
2019-11-07 14:44:55 +0800

    Exam_UI_Menu

---
+++ UnityProject/Assets/GameManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.Audio; // using Audio API(功能)
+//using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+    public AudioMixer mixer;
+    /*public void SetVBGM(float value)
+    {
+        mixer.SetFloat("VBGM", value);
+    }*/
+    public void SetVSFX(float value)
+    {
+        mixer.SetFloat("VSFX", value);
+    }
+}
